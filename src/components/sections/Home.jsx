9823d102afdf1f33f702599cbb3e53a66c7bd6aa
Monil3571 <monilpatel234@gmail.com>
2025-05-29 12:03:46 -0400

    Changed mission statement

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -8,35 +8,39 @@ export const Home = () => {
       id="home"
       className="min-h-screen flex items-center justify-center relative"
     >
-    <RevealOnScroll>
+      <RevealOnScroll>
+        <div className="text-center z-10 px-4">
+          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
+            Hi, I'm Monil Patel
+          </h1>
 
-      <div className="text-center z-10 px-4">
-        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-purple-600 bg-clip-text text-transparent leading-right">
-          Hi, I'm Monil Patel
-        </h1>
-
-        <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
-          Aspiring Web Developer with proficiency in React, Java, HTML, Tailwind
-          CSS, JavaScript, Python, and PHP, seeking to leverage a strong
-          foundation in both front-end and back-end development to create
-          dynamic, user-friendly, and efficient web applications. Committed to
-          continuous learning and growth, I aim to contribute innovative
-          solutions to a forward-thinking tech company while honing my skills in
-          a full-stack development position from May to August 2025.
-        </p>
-        <div className="flex justify-center space-x-4">
-            <a href="#projects" 
-            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 
-            hover:shadow-[0_0_15px_rgba(59,130,236,0.4)]">
-                View Projects
+          <p className="tex-gray-400 text-lg mb-8 max-w-lg mx-auto">
+            Full-Stack Web Developer skilled in the MERN stack (MongoDB,
+            Express.js, React, Node.js), with additional experience in Java,
+            HTML, Tailwind CSS, JavaScript, Python, and PHP. I specialize in
+            building scalable, user-centric web applications that deliver
+            seamless user experiences. Driven by curiosity and a growth mindset,
+            I’m eager to contribute to forward-thinking teams and create
+            impactful digital solutions through clean, efficient, and
+            maintainable code.
+          </p>
+          <div className="flex justify-center space-x-4">
+            <a
+              href="#projects"
+              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 
+            hover:shadow-[0_0_15px_rgba(59,130,236,0.4)]"
+            >
+              View Projects
             </a>
-            <a href="#contact"
-             className=" border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
-             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,236,0.2)] hover:bg-blue-500/10">
-                Contact Me
+            <a
+              href="#contact"
+              className=" border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,236,0.2)] hover:bg-blue-500/10"
+            >
+              Contact Me
             </a>
+          </div>
         </div>
-      </div>
       </RevealOnScroll>
     </section>
   );
