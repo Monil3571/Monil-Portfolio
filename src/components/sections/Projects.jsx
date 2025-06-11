5ede8bc02165376ab7df0ae519c17b9c71fc1cd3
Monil3571 <monilpatel234@gmail.com>
2025-06-11 14:17:55 -0400

    Added the groove landing page project

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -16,27 +16,68 @@ export const Projects = () => {
             <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all ">
               <h3 className="text-xl font-bold mb-2">
                 {" "}
+                The Groove official Landing Page{" "}
+              </h3>
+              <p className="text-gray-400 mb-4">
+                Designed and developed a high-converting landing page for a
+                local bar using Next.js, Tailwind CSS, and Framer Motion.
+                Focused on mobile-first responsive design, performance
+                optimization, and clean UI/UX to reflect the bar’s brand.
+                Delivered a fully functional, production-ready site as part of a
+                real-world client project.
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                {["Nextjs", "React", "TailwindCSS", "Framer-Motion"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
+                "
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a
+                  href="https://the-groove-six.vercel.app/"
+                  className="text-blue-400 hover:text-blue-300 transition-colors my-4"
+                >
+                  {" "}
+                  View Project ➝{" "}
+                </a>
+              </div>
+            </div>
+            <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-blue-500/30 shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all ">
+              <h3 className="text-xl font-bold mb-2">
+                {" "}
                 Layers SaaS Landing Page{" "}
               </h3>
               <p className="text-gray-400 mb-4">
                 This project highlights a clean, modern, and fully responsive
-                landing page designed and developed using Next.js and Tailwind
-                CSS. Emphasizing pixel-perfect design, smooth animations, and
-                mobile-first responsiveness, the landing page demonstrates my
-                ability to create visually appealing and performant web
-                interfaces that deliver excellent user experiences.
+                landing page designed and developed using Next.js, Framer Motion
+                and Tailwind CSS. Emphasizing pixel-perfect design, smooth
+                animations, and mobile-first responsiveness, the landing page
+                demonstrates my ability to create visually appealing and
+                performant web interfaces that deliver excellent user
+                experiences.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Nextjs", "React", "TailwindCSS", "Framer-Motion"].map((tech, key) => (
-                  <span
-                    key={key}
-                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                {["Nextjs", "React", "TailwindCSS", "Framer-Motion"].map(
+                  (tech, key) => (
+                    <span
+                      key={key}
+                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
                 hover:shadow-[0_2px_8px_rgba(59,130,246,0.1)] transition-all
                 "
-                  >
-                    {tech}
-                  </span>
-                ))}
+                    >
+                      {tech}
+                    </span>
+                  )
+                )}
               </div>
 
               <div className="flex justify-between items-center">
